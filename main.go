f6ae398ae2da6c95b83063f2917d90ac25d1a50d
DimaSemenov <zayeryan04@mail.ru>
2025-10-06 19:56:30 +0300

    TRETIY

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"calculator_test/calculator"
+	"calculator_test/shapes"
+	"fmt"
+)
+
+func main() {
+
+	circle1 := shapes.Circle{Radius: 5}
+	circle2 := shapes.Circle{Radius: 10}
+
+	rectangle1 := shapes.Rectangle{Width: 10, Heigh: 10}
+
+	triangle1 := shapes.Triangle{Base: 2, Height: 5}
+
+	circles := []shapes.Circle{circle1, circle2}
+	rectanles := []shapes.Rectangle{rectangle1}
+	triangles := []shapes.Triangle{triangle1}
+
+	totalArea := calculator.TotalArea(circles, triangles, rectanles)
+
+	fmt.Println(totalArea)
+	fmt.Println(circle1)
+
+}
